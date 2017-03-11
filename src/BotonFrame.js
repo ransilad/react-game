8e1f3cd496cf81e9aac53cd06ad0b5fbf5dcdc01
Abel Darío Rangel Silva <ransilad@gmail.com>
2017-03-10 20:47:19 -0400

    Validar respuestas

--- src/BotonFrame.js
+++ src/BotonFrame.js
@@ -3,9 +3,33 @@ import React, { Component } from 'react';
 class BotonFrame extends Component {
   render() {
   	let disabled = (this.props.selectedNum.length == 0);
+  	let button = '';
+  	let correct = this.props.correct;
+
+  	switch(correct){
+  		case true:
+  			button = (
+				<button className="btn btn-success btn-lg">
+					<span className="glyphicon glyphicon-ok"></span>
+				</button>
+  			);
+  			break;
+  		case false:
+  			button = (
+				<button className="btn btn-danger btn-lg">
+					<span className="glyphicon glyphicon-remove"></span>
+				</button>
+  			);
+  			break;
+  		default:
+  			disabled = (this.props.selectedNum.length === 0);
+  			button = (
+				<button className="btn btn-primary btn-lg" disabled={disabled} onClick={this.props.checkAnswer}>=</button>
+  			);
+  	}
     return (
       <div id="boton-frame">
-      	<button className="btn btn-primary btn-lg" disabled={disabled}>=</button>
+      	{button}
       </div>
     );
   }
--- src/Game.js
+++ src/Game.js
@@ -7,9 +7,11 @@ import NumbersFrame from './NumbersFrame';
 class Game extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {numStars: Math.floor(Math.random()*9) + 1, selectedNum:[]};
+		this.state = {numStars: Math.floor(Math.random()*9) + 1, selectedNum:[], correct: null};
 		this.clickNumber = this.clickNumber.bind(this);
 		this.unClickNumber = this.unClickNumber.bind(this);
+		this.checkAnswer = this.checkAnswer.bind(this);
+		this.sumOfSelectedNumbers = this.sumOfSelectedNumbers.bind(this);
 	};
 	clickNumber(clicketNumber){
 		if (this.state.selectedNum.indexOf(clicketNumber) < 0) {
@@ -27,16 +29,26 @@ class Game extends Component {
 			{selectedNum: selectedNum}
 		);
 	};
+	sumOfSelectedNumbers(){
+		return this.state.selectedNum.reduce(function(p,n) {
+			return p+n;
+		},0);
+	};
+	checkAnswer(){
+		let correct = (this.state.numStars === this.sumOfSelectedNumbers());
+		this.setState({correct: correct});
+	};
   	render() {
   		let selectedNum = this.state.selectedNum;
   		let numStars = this.state.numStars;
+  		let correct = this.state.correct;
 	    return (
 	      <div id="game">
 	      	<h2>Play nine</h2>
 	      	<hr />
 	      	<div className="clearfix">
 			  	<StarsFrame numStars={numStars}/>
-			  	<BotonFrame selectedNum={selectedNum} numStars={numStars}/>
+			  	<BotonFrame selectedNum={selectedNum} numStars={numStars} correct={correct} checkAnswer={this.checkAnswer}/>
 			  	<RespFrame selectedNum={selectedNum} unClickNumber={this.unClickNumber}/>
 	      	</div>
 	      	<NumbersFrame selectedNum={selectedNum} clickNumber={this.clickNumber}/>
